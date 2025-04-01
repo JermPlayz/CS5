c77f5927a2c3359d3ad13acea2a6191510a94b2b
JermPlayz <jeremycschoonover@icloud.com>
2025-04-01 11:01:54 -0500

    Objects

--- Assets/Scenes/MapScene/Scripts/CharacterCtrl.cs
+++ Assets/Scenes/MapScene/Scripts/CharacterCtrl.cs
@@ -4,17 +4,17 @@ public class CharacterCtrl : MonoBehaviour
 {
     public int character;
     Vector2 worldPoint;
-     RaycastHit2D hit;
+    RaycastHit2D hit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int zero = 0;
     }
      // Update is called once per frame
      void Update()
      {
 
-         
+         int zero = 0;
      }
 }
