2cc986142ef0b2f9a402ae8290958f7f0d436171
UnderlyingNerd <tygabino@gmail.com>
2025-04-01 10:32:30 -0500

    Update GameCtrl.cs

--- Assets/Scenes/GameCtrl.cs
+++ Assets/Scenes/GameCtrl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState {MOVEMENT, ACTION, COMBAT}
+public enum GameState {SELECTOR, MOVEMENT, ACTION, COMBAT}
 
 public class GameCtrl : MonoBehaviour
 {
@@ -21,5 +21,18 @@ public class GameCtrl : MonoBehaviour
         {
             
         }
+
+        worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+         if (Input.GetMouseButtonDown(0))
+         {
+             hit = Physics2D.Raycast(worldPoint, Vector2.down);
+
+             if (hit.collider != null)
+             {
+                Debug.Log("click on " + hit.collider.name);
+                Debug.Log(hit.point);
+             } 
+         }     
     }
 }
