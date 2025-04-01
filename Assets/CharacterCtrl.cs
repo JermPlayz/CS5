3c9f894136d73bc5dd8a32dba564c26bf05747b2
UnderlyingNerd <tygabino@gmail.com>
2025-04-01 10:31:38 -0500

    a

--- Assets/CharacterCtrl.cs
+++ Assets/CharacterCtrl.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class CharacterCtrl : MonoBehaviour
 {
     public int character;
+    Vector2 worldPoint;
+     RaycastHit2D hit;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+     // Update is called once per frame
+     void Update()
+     {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+         
+     }
 }
