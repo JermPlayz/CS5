9d3fc9a7d4e7b8cb89916857589a66ce3972a5e8
JermPlayz <jeremycschoonover@icloud.com>
2025-04-15 10:56:21 -0500

    Audio Stuff

---
+++ Assets/Scenes/MapScene/InputManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.Instance.ChangeMusic(AudioManager.SoundType.Music_Menu);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            AudioManager.Instance.ChangeMusic(AudioManager.SoundType.Music_Battle);
+        }
+        else if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            AudioManager.Instance.Play(AudioManager.SoundType.SelectAction);
+        }
+    }
+}
